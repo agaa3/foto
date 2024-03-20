f5a4cd84eb1b497aa437ec3b5c4be16db4c666c7
Agata Granosik <agatagranosik3@gmail.com>
2024-03-20 15:17:20 +0100

    sfml

--- project/project/project.cpp
+++ project/project/project.cpp
@@ -12,11 +12,32 @@
 #include "Sphere.h"
 #include "Triangle.h"
 #include "ObjectOnScene.h"
+#include <SFML/Graphics.hpp>
 
 const int SIZE = 60;
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+
+
     //1
     //2
     Vector3 v1 = Vector3(1, 2, 3);
